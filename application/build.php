f03b7114127be22536ff7e665bb978f2e0275c1f
Sefa <3472933148@qq.com>
2019-01-05 13:58:47 +0800

    创建 api 模块

---
+++ application/build.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sefa
+ * Date: 2019/1/5
+ * Time: 9:58
+ */
+return [
+    // 定义 api 模块的自动生成文件
+    'api' => [
+        '__file__' => ['common.php'],
+        '__dir__' => ['behavior', 'controller', 'model'],
+
+        'controller' => ['Index', 'User'],
+        'model' => ['User'],
+    ]
+];
