b7e3cc3be3499981d6b94664828c72d02d6c9aab
Sefa <3472933148@qq.com>
2019-01-10 17:10:20 +0800

    修复Bug

--- application/api/service/BaseService.php
+++ application/api/service/BaseService.php
@@ -18,7 +18,7 @@ class BaseService
     public static function generateToken()
     {
         $randChars = getRandChar(32);
-        $timestamp = $_SERVER('REQUEST_TIME_FLOAT');
+        $timestamp = $_SERVER['REQUEST_TIME_FLOAT'];
         $salt = config('secure.token_salt');
 
         return md5($randChars.$timestamp.$salt);
