89e9ddd339c46c74aeb9fdaa882573e0327b019a
Sefa <3472933148@qq.com>
2019-01-16 08:34:39 +0800

    支付接口完成一半，进行调试

---
+++ application/api/service/Pay.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sefa
+ * Date: 2019/1/15
+ * Time: 16:17
+ */
+
+namespace app\api\service;
+
+
+class Pay
+{
+
+}
---
+++ application/lib/enum/OrderStatusEnum.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sefa
+ * Date: 2019/1/15
+ * Time: 17:14
+ */
+
+namespace app\lib\enum;
+
+
+class OrderStatusEnum
+{
+
+}
